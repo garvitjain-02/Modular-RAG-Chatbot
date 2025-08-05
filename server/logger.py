ce94883e653097d71d299983f6c1de8207b6c040
Garvit Jain <garvitjainjnv@gmail.com>
2025-08-05 10:16:37 +0530

    updated setup of logger

--- server/logger.py
+++ server/logger.py
@@ -1,17 +1,17 @@
+# used to check all kind of warning,information and error messages that we might need for debugging
 import logging
 
 
 def setup_logger(name="ragbot"):
 
     logger=logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG) #what reason we want to use log file -> that is debugging
 
     # Console handler
-    ch=logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    ch=logging.StreamHandler() #send and show all the logs to console
+    ch.setLevel(logging.DEBUG) # set the level of the console handler to debug
 
-    # formatter
-
+    # formatter (in which format we want to see the logs)
     formatter=logging.Formatter("[%(asctime)s] [%(levelname)s] -  %(message)s ")
     ch.setFormatter(formatter)
 
